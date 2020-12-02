28382b4ba7406de3eb73b5e50c74b8734ea552b1
KateL <48454133+KateL9@users.noreply.github.com>
2020-12-02 19:55:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,75 @@
-/*Выведите на страницу текущую дату и время в 2 - х форматах:
-a)
-'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
-б)
-'04.02.2020 - 21:05:33'
-2) Для вывода в формате(а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
-3) Для вывода в формате(б) напишите функцию, которая будет добавлять 0 перед значениями 
-которые состоят из одной цифры(из 9: 5: 3 1.6 .2019 сделает 09: 05: 03 01.06 .2019)
-4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду*/
+function getWeekDay(date) {
+  const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота"
+  ];
+
+  return days[date.getDay()];
+}
+
+function getHourWord(el) {
+  if (el == 1 || el == 21) {
+    return "час";
+  } else if (
+    el == 2 ||
+    el == 3 ||
+    el == 4 ||
+    el == 22 ||
+    el == 23 ||
+    el == 24
+  ) {
+    return "часа";
+  }
+  return " часов ";
+}
 
 function getDateAndTime() {
-    window.location.reload();
-    let date = new Date(),
-        day = date.getDate(),
-        month = date.getMonth() + 1,
-        year = date.getFullYear(),
-        hours = date.getHours(),
-        minutes = date.getMinutes(),
-        seconds = date.getSeconds(),
-        arr = [day, month, year, hours, minutes, seconds];
-    // меняем тип данных на string
-    arr = arr.map(function(element) {
-        return element.toString();
-    });
-    // Добавляем 0
-    arr = arr.map(function(element) {
-        if (element.length <= 1) {
-            return element = '0' + element;
-        }
-        return element;
-    });
+  const date = new Date(),
+    day = date.getDate(),
+    month = date.getMonth() + 1,
+    year = date.getFullYear(),
+    hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds();
+  const today = getWeekDay(date);
+  const curentMonth = date.getMonth();
+  const months = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря"
+  ];
+  const hoursWord = getHourWord(hours);
+  const dateEl = document.getElementById("firstDateFormat");
+  const dateFullEl = document.getElementById("secondDateFormat");
+  let arr = [day, month, year, hours, minutes, seconds];
 
-    //Определяем день недели совом
-    function getWeekDay(date) {
-        let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-        return days[date.getDay()];
-    };
-    let today = getWeekDay(date);
+  arr = arr.map(function (element) {
+    return element.toString();
+  });
 
-    //Определяем месяц словом
-    let curentMonth = date.getMonth(),
-        months = [];
-    months[0] = "Января";
-    month[1] = "Февраля";
-    months[2] = "Марта";
-    months[3] = "Апреля";
-    months[4] = "Мая";
-    months[5] = "Июня";
-    months[6] = "Июля";
-    months[7] = "Августа";
-    months[8] = "Сентября";
-    months[9] = "Октября";
-    months[10] = "Ноября";
-    months[11] = "Декабря";
+  arr = arr.map(function (element) {
+    if (element.length <= 1) {
+      return (element = "0" + element);
+    }
+    return element;
+  });
 
-    //Определяем часы словом
-    let hoursWord;
-
-    function getHourWord(el) {
-        if (el == 1 || el == 21) {
-            hoursWord = 'час';
-        } else if (el == 2 || el == 3 || el == 4 || el == 22 || el == 23 || el == 24) {
-            hoursWord = 'часа';
-        }
-        hoursWord = ' часов ';
-    };
-    getHourWord(hours);
-
-    //Вывод результатов
-    document.write(arr[0] + '.' + arr[1] + '.' + arr[2] + ' - ' + arr[3] + ':' + arr[4] + ':' + arr[5] + '<br/>');
-    document.write('Сегодня ' + today + ' ' + arr[0] + months[curentMonth] + ' ' + arr[2] + ' год ' + arr[3] + ' ' + hoursWord + arr[4] + ' минут ' + arr[5] + ' секунд.');
-};
-
+  dateEl.innerText = `${arr[0]}.${arr[1]}.${arr[2]} - ${arr[3]}:${arr[4]}:${arr[5]}`;
+  dateFullEl.innerText = `Сегодня ${today} ${arr[0]} ${months[curentMonth]} ${arr[2]} год ${arr[3]} ${hoursWord}${arr[4]} минут ${arr[5]} секунд.`;
+}
 
 setInterval(getDateAndTime, 1000);
