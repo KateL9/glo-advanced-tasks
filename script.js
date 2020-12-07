9b4491001d4db81bec49737bcac9ab2252b959ec
Kate Kunnikova <levchenkokaite@gmail.com>
2020-12-08 00:24:25 +0200

    init: random color

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-//Создать переменную num со значением 266219(тип данных число).
-let num = 266219,
-    multiple = 1,
-    arr = String(num).split('');
+const color = document.querySelector('#color'),
+    btn = document.querySelector('#change');
 
-//Вывести в консоль произведение(умножение) цифр этого числа.
-for (i = 0; i < arr.length; i++) {
-    multiple *= arr[i];
-};
-console.log(multiple);
+function getRandomColor() {
+    color.textContent = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = color.textContent;
+    btn.style.color = color.textContent;
+}
 
-//Полученный результат возвести в степень 3.
-let pow = multiple ** 3;
-
-//Вывести на экран первые 2 цифры полученного числа.
-alert(String(pow).substr(0, 2));
+btn.addEventListener('click', getRandomColor);
