11db5816e2df8b2b7fe8cdf350f28d4084bbb096
Kate Kunnikova <levchenkokaite@gmail.com>
2020-12-09 16:27:33 +0200

    init: moving square

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
-//Создать переменную num со значением 266219(тип данных число).
-let num = 266219,
-    multiple = 1,
-    arr = String(num).split('');
+/* 1) Используя class DomElement из основного задания №1, 
+создать квадрат 100 на 100 пикселей. Ему необходимо задать фон(background) любого цвета и свойство position: absolute.
+2) Поместить его на страницу только после выполнения события DOMContentLoaded.
+Внутри тега body  должно быть только подключение скрипта.
+3) Написать обработчик события для keydown, который будет принимать callback-функцию. 
+Данная функция будет отлавливать нажатие на стрелки клавиатуры. В зависимости от нажатой кнопки
+(Вверх - стрелка вверх, Влево - стрелка влево, Вправо - стрелка вправо, Вниз - стрелка вниз) наш квадрат будет перемещаться на 10 пикселей. */
 
-//Вывести в консоль произведение(умножение) цифр этого числа.
-for (i = 0; i < arr.length; i++) {
-    multiple *= arr[i];
+
+function DomElement(selector, height, width, bg) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.createElem = function(selector) {
+        document.addEventListener("DOMContentLoaded", () => {
+
+            first = this.selector.slice(0, 1);
+            let selectorName = this.selector.slice(1);
+            if (first == '.') {
+                //create div .block 
+                document.body.insertAdjacentHTML("afterbegin", `<div class=${selectorName}></div>`);
+                div = document.querySelector('div');
+                div.style.cssText = `
+                height: ${this.height}px;
+                background-color: ${bg};
+                width: ${width}px;
+                position: absolute;`;
+                let topPx = 0,
+                    left = 0;
+
+                document.addEventListener("keydown", function(evet) {
+                    event.preventDefault();
+                    let key = event.which;
+                    switch (key) {
+                        case 38: //arrow up
+                            topPx = topPx - 10;
+                            div.style.top = topPx + "px";
+                            break;
+                        case 40: //arrow down
+                            topPx = topPx + 10;
+                            div.style.top = topPx + "px";
+                            break;
+                        case 39: //arrow right
+                            left = left + 10;
+                            div.style.left = left + "px";
+                            break;
+                        case 37: //arrow left
+                            left = left - 10;
+                            div.style.left = left + "px";
+                            break;
+                    }
+                });
+
+            } else if (first == '#') {
+                //create p #best
+                document.body.insertAdjacentHTML("afterbegin", `<p id=${selectorName}></p>`);
+                let p = document.querySelector('p');
+                p.style.cssText = `
+                height: ${this.height}px;
+                background-color: ${this.bg};
+                width: ${this.width}px;
+                position: absolute;`;
+            }
+
+        });
+    }
 };
-console.log(multiple);
 
-//Полученный результат возвести в степень 3.
-let pow = multiple ** 3;
-
-//Вывести на экран первые 2 цифры полученного числа.
-alert(String(pow).substr(0, 2));
+let obj = new DomElement('.', '100', '100', 'red');
+obj.createElem();
