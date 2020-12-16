68bceb8bade55cd780c499431ae1af637a085761
Kate Kunnikova <levchenkokaite@gmail.com>
2020-12-16 22:11:57 +0200

    init: debouncing

---
+++ lesson18.1/script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#text'),
+    paragraph = document.querySelector('#p');
+
+const debounce = (func, wait, immediate) => {
+    let timeout;
+
+    return function executedFunction() {
+        let context = this;
+        let args = arguments;
+
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        let callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+};
+input.addEventListener('keyup', debounce(function() {
+    paragraph.textContent = input.value;
+}, 300));
